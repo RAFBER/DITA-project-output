8b591f7f2239fea54d0a2c841bd87d73df5871d6
RAFAL BERTOLASSI <78052874+RAFBER@users.noreply.github.com>
2021-03-26 12:02:54 +0100

    PDF file added

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,14 +1,15 @@
-define(function() {var keywords=[{w:"My",p:["p0","p1","p2","p3","p4","p5","p7","p8"]},{w:"first",p:["p0","p4"]},{w:"concept",p:["p0","p1","p2","p3"]},{w:"fourth",p:["p1"]},{w:"second",p:["p2","p5","p7"]},{w:"third",p:["p3","p8"]},{w:"reference",p:["p4","p5"]},{w:"topic",p:["p4"]},{w:"Showing",p:["p6"]},{w:"full",p:["p6"]},{w:"tags",p:["p6"]},{w:"in",p:["p6"]},{w:"the",p:["p6"]},{w:"oXygen",p:["p6"]},{w:"author",p:["p6"]},{w:"view",p:["p6"]},{w:"by",p:["p6"]},{w:"default",p:["p6"]},{w:"task",p:["p7","p8"]}];
+define(function() {var keywords=[{w:"My",p:["p0","p1","p2","p3","p5","p6","p8","p9"]},{w:"first",p:["p0","p5"]},{w:"concept",p:["p0","p1","p2","p3"]},{w:"fourth",p:["p1"]},{w:"second",p:["p2","p6","p8"]},{w:"third",p:["p3","p9"]},{w:"Downloads",p:["p4"]},{w:"reference",p:["p5","p6"]},{w:"topic",p:["p5"]},{w:"Showing",p:["p7"]},{w:"full",p:["p7"]},{w:"tags",p:["p7"]},{w:"in",p:["p7"]},{w:"the",p:["p7"]},{w:"oXygen",p:["p7"]},{w:"author",p:["p7"]},{w:"view",p:["p7"]},{w:"by",p:["p7"]},{w:"default",p:["p7"]},{w:"task",p:["p8","p9"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[0, 3, 2];
 ph["p2"]=[0, 4, 2];
 ph["p3"]=[0, 5, 2];
-ph["p4"]=[0, 1, 6, 7];
-ph["p5"]=[0, 4, 6];
-ph["p6"]=[8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
-ph["p7"]=[0, 4, 18];
-ph["p8"]=[0, 5, 18];
+ph["p4"]=[6];
+ph["p5"]=[0, 1, 7, 8];
+ph["p6"]=[0, 4, 7];
+ph["p7"]=[9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+ph["p8"]=[0, 4, 19];
+ph["p9"]=[0, 5, 19];
      return {
          keywords: keywords,
          ph: ph
--- oxygen-webhelp/app/search/index/link-to-parent.js
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {8:1,5:1,1:-1,0:-1,2:-1,4:0,7:1,6:0,3:-1};
+return {4:-1,2:-1,8:1,0:-1,3:-1,9:1,6:1,5:0,1:-1,7:0};
 });
