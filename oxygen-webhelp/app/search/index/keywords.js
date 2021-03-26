1dc7c965e6a15984643a3ab30d7b94701b5ce456
RAFAL BERTOLASSI <78052874+RAFBER@users.noreply.github.com>
2021-03-26 10:22:27 +0100

    Topics added

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,10 +1,11 @@
-define(function() {var keywords=[{w:"My",p:["p0","p1","p2","p3"]},{w:"first",p:["p0","p3"]},{w:"concept",p:["p0","p1","p2"]},{w:"second",p:["p1"]},{w:"third",p:["p2"]},{w:"reference",p:["p3"]},{w:"topic",p:["p3"]},{w:"Showing",p:["p4"]},{w:"full",p:["p4"]},{w:"tags",p:["p4"]},{w:"in",p:["p4"]},{w:"the",p:["p4"]},{w:"oXygen",p:["p4"]},{w:"author",p:["p4"]},{w:"view",p:["p4"]},{w:"by",p:["p4"]},{w:"default",p:["p4"]}];
+define(function() {var keywords=[{w:"My",p:["p0","p1","p2","p3","p4"]},{w:"first",p:["p0","p4"]},{w:"concept",p:["p0","p1","p2","p3"]},{w:"fourth",p:["p1"]},{w:"second",p:["p2"]},{w:"third",p:["p3"]},{w:"reference",p:["p4"]},{w:"topic",p:["p4"]},{w:"Showing",p:["p5"]},{w:"full",p:["p5"]},{w:"tags",p:["p5"]},{w:"in",p:["p5"]},{w:"the",p:["p5"]},{w:"oXygen",p:["p5"]},{w:"author",p:["p5"]},{w:"view",p:["p5"]},{w:"by",p:["p5"]},{w:"default",p:["p5"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[0, 3, 2];
 ph["p2"]=[0, 4, 2];
-ph["p3"]=[0, 1, 5, 6];
-ph["p4"]=[7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+ph["p3"]=[0, 5, 2];
+ph["p4"]=[0, 1, 6, 7];
+ph["p5"]=[8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
      return {
          keywords: keywords,
          ph: ph
--- oxygen-webhelp/app/search/index/link-to-parent.js
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {3:0,4:0,1:-1,2:-1,0:-1};
+return {3:-1,0:-1,4:0,1:-1,5:0,2:-1};
 });
