b48d56d27a5b208121a15b72eaf11a51a0e830bd
RAFAL BERTOLASSI <78052874+RAFBER@users.noreply.github.com>
2021-03-26 10:06:48 +0100

    Third topic added

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,9 +1,10 @@
-define(function() {var keywords=[{w:"My",p:["p0","p1","p2"]},{w:"first",p:["p0","p2"]},{w:"concept",p:["p0","p1"]},{w:"second",p:["p1"]},{w:"reference",p:["p2"]},{w:"topic",p:["p2"]},{w:"Showing",p:["p3"]},{w:"full",p:["p3"]},{w:"tags",p:["p3"]},{w:"in",p:["p3"]},{w:"the",p:["p3"]},{w:"oXygen",p:["p3"]},{w:"author",p:["p3"]},{w:"view",p:["p3"]},{w:"by",p:["p3"]},{w:"default",p:["p3"]}];
+define(function() {var keywords=[{w:"My",p:["p0","p1","p2","p3"]},{w:"first",p:["p0","p3"]},{w:"concept",p:["p0","p1","p2"]},{w:"second",p:["p1"]},{w:"third",p:["p2"]},{w:"reference",p:["p3"]},{w:"topic",p:["p3"]},{w:"Showing",p:["p4"]},{w:"full",p:["p4"]},{w:"tags",p:["p4"]},{w:"in",p:["p4"]},{w:"the",p:["p4"]},{w:"oXygen",p:["p4"]},{w:"author",p:["p4"]},{w:"view",p:["p4"]},{w:"by",p:["p4"]},{w:"default",p:["p4"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[0, 3, 2];
-ph["p2"]=[0, 1, 4, 5];
-ph["p3"]=[6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+ph["p2"]=[0, 4, 2];
+ph["p3"]=[0, 1, 5, 6];
+ph["p4"]=[7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
      return {
          keywords: keywords,
          ph: ph
