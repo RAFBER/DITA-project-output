82b6dafdb2db8e6e8d45f55001fdf3fddbde850b
RAFAL BERTOLASSI <78052874+RAFBER@users.noreply.github.com>
2021-03-26 10:42:52 +0100

    Update

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210326102053';
+                args = '20210326103851';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
--- oxygen-webhelp/app/search/index/link-to-parent.js
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {3:-1,0:-1,4:0,1:-1,5:0,2:-1};
+return {8:1,5:1,1:-1,0:-1,2:-1,4:0,7:1,6:0,3:-1};
 });
